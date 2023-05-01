dfdc18f126d736caf8999a20309aad64f007006f
Sunday ejembi <s.ejembi@gmail.com>
2023-05-01 04:12:42 -0700

    all task done

--- models/user.py
+++ models/user.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ holds class User"""
+
 import models
-import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import hashlib
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -29,4 +29,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
-    
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)

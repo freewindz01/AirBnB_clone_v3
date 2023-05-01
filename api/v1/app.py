7dfaea1d65d529755a17e9786388fd5dda0e4797
Sunday ejembi <s.ejembi@gmail.com>
2023-05-01 03:23:46 -0700

    all task done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""Flask Application Server"""
-from flask import Flask
+"""This module contain a flask application that deals with APIs"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def disconnect(var):
-    """Close connection to storage engine"""
+def close(exception):
+    """Call storage.close to close a session and get another"""
     storage.close()
 
+@app.errorhandler(404)
+def handle404(error):
+    """Handle error, 404"""
+    return make_response(jsonify({ "error": "Not found" }), 404)
 
-if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=getenv('HBNB_API_PORT', 5000),
-            threaded=True)
+
+
+if __name__ == '__main__':
+    import os
+    host = os.getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = os.getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True)
